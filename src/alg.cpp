837a367e94391da71ddf1d3a9cdc87bc21fab928
kostykrasil <163845822+kostykrasil@users.noreply.github.com>
2026-04-05 23:50:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,98 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len - 1; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        
+        if (sum == value) {
+            if (arr[left] == arr[right]) {
+                int n = right - left + 1;
+                count += (n * (n - 1)) / 2; 
+                break;
+            } else {
+                int c_left = 1;
+                while (left + 1 < right && arr[left] == arr[left + 1]) {
+                    c_left++;
+                    left++;
+                }
+                int c_right = 1;
+                while (right - 1 > left && arr[right] == arr[right - 1]) {
+                    c_right++;
+                    right--;
+                }
+                
+                count += c_left * c_right;
+                left++;
+                right--;
+            }
+        } else if (sum < value) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return count;
 }
+
+int findFirst(int *arr, int left, int right, int target) {
+    int res = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            res = mid;
+            right = mid - 1; 
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return res;
+}
+
+int findLast(int *arr, int left, int right, int target) {
+    int res = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            res = mid;
+            left = mid + 1; 
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return res;
+}
+
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len - 1; ++i) {
+        int target = value - arr[i];
+        
+        int first = findFirst(arr, i + 1, len - 1, target);
+        
+        if (first != -1) {
+            int last = findLast(arr, i + 1, len - 1, target);
+            count += (last - first + 1);
+        }
+    }
+    return count;
 }
